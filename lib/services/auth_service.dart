59906600a781afd53629532aa8236b9285c62561
Mohammad Ali Khan <xvirus.bd@gmail.com>
2022-02-03 00:36:36 +0600

    adding try catch to register and login function

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,17 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 class authService {
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   Future<User?> register(String email, String password)async{
-    UserCredential userCredential = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-   return userCredential.user;
+    try {
+      UserCredential userCredential = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    }on FirebaseAuthException catch (e) {
+      SnackBar(content: Text(e.message.toString()),backgroundColor: Colors.red,);
+    }catch (e){
+      print(e);
+    }
   }
 
   Future<User?> login(String email, String password)async{
-    UserCredential userCredential = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-   return userCredential.user;
+    try {
+      UserCredential userCredential = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    }on FirebaseAuthException catch (e) {
+      SnackBar(content: Text(e.message.toString()),backgroundColor: Colors.red,);
+    }catch (e){
+      print(e);
+    }
   }
 
 }
