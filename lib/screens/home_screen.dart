39e62f357606b870368e1323423b6a7cf1d1e433
Mohammad Ali Khan <xvirus.bd@gmail.com>
2022-02-24 20:40:14 +0600

    retrieve data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/note.dart';
 import 'package:flutter_firebase/screens/add_note.dart';
 import 'package:flutter_firebase/screens/edit_note.dart';
 import 'package:flutter_firebase/services/auth_service.dart';
@@ -22,24 +23,37 @@ class Homescreen extends StatelessWidget {
           }, icon: Icon(Icons.logout), label: Text('Sign Out')),
         ],
       ),
-      body: ListView(
-        children: [
-          Card(
-            color: Colors.teal,
-            elevation: 5,
-            margin: EdgeInsets.all(10),
-            child: ListTile(
-              contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-              title: Text('Build a new app',style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-              subtitle: Text('Learn to build',overflow: TextOverflow.ellipsis,maxLines: 2,),
-            onTap: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>EditNoteScreen()));
-            },
-                
-            ),
-          ),
-        ],
-      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('notes').where('userId', isEqualTo: user.uid).snapshots(),
+        builder: (contex, AsyncSnapshot snapshot){
+          if(snapshot.hasData){
+            if(snapshot.data.docs.length > 0){
+              return ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (contex,index){
+                  NoteModel note = NoteModel.fromJson(snapshot.data.docs[index]);
+                  return Card(
+                        color: Colors.teal,
+                        elevation: 5,
+                        margin: EdgeInsets.all(10),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                          title: Text(note.title.toString(),style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                          subtitle: Text(note.description.toString(),overflow: TextOverflow.ellipsis,maxLines: 2,),
+                          onTap: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>EditNoteScreen()));
+                          },
+                        ),
+                      );
+                 }
+                );
+            }else{
+              return Center(child: Text('No Notes available'),);
+            }
+          }
+          return Center(child: CircularProgressIndicator(),);
+        }
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
